e818755001c730895874d1850df3b1a7744d9ca4
Tquyh <quynhpvt21411c@st.uel.edu.vn>
2023-11-14 00:35:05 +0700

    Create contact.component.ts

---
+++ Coding_Angula/my-app/src/app/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent {
+  public companyName:string="K21411C"
+  public companyEmail="k21411c@st.uel.edu.vn"
+  public companyPhone:String="113"
+}
